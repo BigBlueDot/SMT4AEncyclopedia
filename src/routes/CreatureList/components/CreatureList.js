327385b56050bccc195d044b6152c1fbbcf8d328
BigBlueDot <BigBlueDotGit@gmail.com>
2016-12-14 20:55:28 -0600

    Added basic printing of demons

--- src/routes/CreatureList/components/CreatureList.js
+++ src/routes/CreatureList/components/CreatureList.js
@@ -2,12 +2,21 @@
  * Created by bigbl on 12/14/2016.
  */
 import React from 'react';
+import demon from '../../../data/demon.json';
 
-export const CreatureList = (props) => (
-  <div style={{ margin: '0 auto' }} >
-    Creature List will go here
-  </div>
-);
+export const CreatureList = (props) => {
+  let creatures = [];
+  let index = 0;
+  demon.demons.forEach((d) => {
+    index++;
+    creatures.push(<p key={index}>{d.Name}</p>);
+  });
+  return (
+    <div style={{ margin: '0 auto' }} >
+      {creatures}
+    </div>
+  );
+}
 
 CreatureList.propTypes = {
 };
