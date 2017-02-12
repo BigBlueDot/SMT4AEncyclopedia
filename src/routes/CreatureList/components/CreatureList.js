b1cf25ee2bb9a90c007d0aabe1c6a9dfd3984970
BigBlueDot <BigBlueDotGit@gmail.com>
2017-02-11 19:41:08 -0600

    Did some fixing of the grid display, added a charting framework

--- src/routes/CreatureList/components/CreatureList.js
+++ src/routes/CreatureList/components/CreatureList.js
@@ -5,10 +5,82 @@ import React from 'react';
 import demon from '../../../data/demon.json';
 import Griddle from 'griddle-react';
 
+const columnMetadata = [{
+  columnName: "Name",
+  displayName: "Name",
+  sortable: true,
+},{
+  columnName: "Level",
+  displayName: "Level",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "Race",
+  displayName: "Race",
+  sortable: true,
+},{
+  columnName: "HP",
+  displayName: "HP",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "MP",
+  displayName: "MP",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "Strength",
+  displayName: "Strength",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "Dexterity",
+  displayName: "Dexterity",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "Magic",
+  displayName: "Magic",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "Agility",
+  displayName: "Agility",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+},{
+  columnName: "Luck",
+  displayName: "Luck",
+  sortable: true,
+  customCompareFn: function(item) {
+    return parseFloat(item);
+  }
+}];
+
 export const CreatureList = (props) => {
   return (
     <div style={{ margin: '0 auto' }} >
-      <Griddle results={demon.demons} />
+      <Griddle
+        results={demon.demons}
+        enableInfiniteScroll={true}
+        bodyHeight={400}
+        useFixedHeader={true}
+        showFilter={true}
+        columnMetadata={columnMetadata} />
     </div>
   );
 }
