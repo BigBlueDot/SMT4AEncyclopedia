f287acff7963b7cf137bbc12938bfcac0142ce72
BigBlueDot <BigBlueDotGit@gmail.com>
2017-02-12 18:11:33 -0600

    Added sample chart for data page

---
+++ src/routes/Data/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by bigbl on 12/14/2016.
+ */
+import { injectReducer } from '../../store/reducers';
+
+export default (store) => ({
+  path : 'data',
+  /*  Async getComponent is only invoked when route matches   */
+  getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+     and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+       dependencies for bundling   */
+      const Data = require('./containers/DataContainer').default;
+      const reducer = require('./modules/data').default;
+
+      /*  Add the reducer to the store on key 'counter'  */
+      injectReducer(store, { key: 'data', reducer });
+
+      /*  Return getComponent   */
+      cb(null, Data);
+
+      /* Webpack named bundle   */
+    }, 'data');
+  }
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import CoreLayout from '../layouts/CoreLayout/CoreLayout';
 import Home from './Home';
 import CounterRoute from './Counter';
 import CreatureListRoute from './CreatureList';
+import DataRoute from './Data';
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
@@ -13,7 +14,8 @@ export const createRoutes = (store) => ({
   indexRoute  : Home,
   childRoutes : [
     CounterRoute(store),
-    CreatureListRoute(store)
+    CreatureListRoute(store),
+    DataRoute(store),
   ]
 });
 
