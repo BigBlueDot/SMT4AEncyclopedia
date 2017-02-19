a25cf76f0838ab238f95722bbcebc0dfef3c6ecc
BigBlueDot <BigBlueDotGit@gmail.com>
2017-02-19 12:03:32 -0600

    Removed unnecessary functions

--- src/routes/Data/components/Data.js
+++ src/routes/Data/components/Data.js
@@ -33,20 +33,6 @@ const getDataObject = (dataLabel, labels, data) => {
   }
 };
 
-const getLevelLabels = (demons) => {
-  // find and min max level
-  let min = 1;
-  let max = 99;
-
-   // generate label list
-  let levels = [];
-  for (var i=min; i<=max; i++) {
-    levels.push(i.toString());
-  }
-
-  return levels;
-};
-
 const skipLevelLabels = (levelLabels) => {
   let newLabels = levelLabels.map((label) => {
     if (parseInt(label) % 5 === 0) {
@@ -57,85 +43,6 @@ const skipLevelLabels = (levelLabels) => {
     }
   });
   return newLabels;
-};
-
-const getValuesForLevel = (demons, levelLabels, statName) => {
-   // create empty array for each level
-  let fullData = {};
-  levelLabels.forEach((l) => {
-    fullData[l] = [];
-  });
-
-  // go through each demon and categorize the level
-  demons.forEach((d) => {
-    if (fullData[d.Level.toString()]) fullData[d.Level.toString()].push(d[statName]);
-  });
-
-  return fullData;
-};
-
-const getAverageValueForLevel = (levelValues) => {
-  // go through and get the average for each level
-  let results = {};
-  Object.keys(levelValues).forEach((key) => {
-    const values = levelValues[key];
-    let sum = 0;
-    values.forEach((v) => {
-      sum += v;
-    });
-
-    const avg = sum / values.length;
-    results[key] = isNaN(avg) ? 0 : sum / values.length;
-  });
-
-  return results;
-};
-
-const convertToArray = (values) => {
-  let results = [];
-  Object.keys(values).forEach((key) => {
-    results[parseInt(key)] = values[key];
-  });
-  return results;
-};
-
-const getMovingAverage = (data, legSize) => {
-  // get the moving average 1-1 with the data by gradually increasing the window size
-  let results = [];
-
-  // verify that the windowSize works with the length of data passed in
-  if ((legSize * 2 + 1) > data.length) {
-    return data;
-  };
-
-  // get the data from the beginning
-  for (var i=1; i<=legSize; i++) {
-    let currentValue = 0;
-    for (var j=1; j <= i * 2; j++) {
-      currentValue += parseFloat(data[j]);
-    }
-    results[i] = currentValue / (i * 2);
-  }
-
-  // get the middle data
-  for (var i=legSize + 1; i < data.length - legSize; i++) {
-    let currentValue = 0;
-    for (var j=i - legSize; j <= i + legSize; j++) {
-      currentValue += data[j];
-    }
-    results[i] = currentValue / (legSize * 2 + 1)
-  }
-
-  // get the end data
-  for (var i=data.length - legSize; i<data.length; i++) {
-    let currentValue = 0;
-    for (var j=i - (data.length - i - 1); j < data.length; j++) {
-      currentValue += data[j];
-    }
-    results[i] = currentValue / ((data.length - i - 1) * 2 + 1);
-  }
-
-  return results;
 };
 
 class Data extends React.Component {
